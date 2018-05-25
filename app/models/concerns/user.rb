ecfc19e7f6c447bd64f8ba3a416d98ea47820ab4
mwzaman <mozaman.knit@gmail.com>
2018-05-25 11:32:04 +0000

    Add secure password

--- app/models/concerns/user.rb
+++ app/models/concerns/user.rb
@@ -6,6 +6,7 @@ validates :username, presence: true, uniqueness: { case_sensitive: false},
 VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i        
 validates :email, presence: true, uniqueness: { case_sensitive: false},
            length: {maximum: 105 }, format: { with: VALID_EMAIL_REGEX,
-    message: "Only allows letters" }
+           message: "Only allows letters" }
+has_secure_password
              
 end
