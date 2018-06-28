ef44443bb35bf8fa70217637bc92fd1623de59da
Md Ohidurzaman <zamanohidur@yahoo.com>
2018-06-28 10:47:09 +0000

    Update admin

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
     
  def setup
-    @user = User.create(username: "fath", email: "fath@gmail.com", password: "password", admin: true)
+    @user = User.create(username: "fatima", email: "fatima.knit@gmail.com", password: "mother", admin: true)
  end
     
     test "get new category form and create category" do
